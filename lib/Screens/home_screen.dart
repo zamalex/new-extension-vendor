7d5056de4bce1a108cabd083f196208b3fe68903
Amal Elgalant <aelgalant@badee.com.sa>
2021-06-30 11:37:40 +0200

    change bootm bar icons

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:salon_vendor/Screens/Notification/NotificationList.dart';
@@ -6,84 +5,84 @@ import 'package:salon_vendor/Screens/appoinment/appointment_list.dart';
 import 'package:salon_vendor/Screens/order/orders_list.dart';
 
 class Home extends StatefulWidget {
-  
- @override
- State<StatefulWidget> createState() {
+  @override
+  State<StatefulWidget> createState() {
     return _HomeState();
   }
 }
 
 class _HomeState extends State<Home> {
-
   int _selectedIndex = 0; //New
-List<Widget> _pages = <Widget>[
+  List<Widget> _pages = <Widget>[
     AppointmentList(),
     OrderList(),
     UserNotificationsList(),
-  
   ];
   void _onItemTapped(int index) {
-  setState(() {
-    _selectedIndex = index;
-  });
-}
- @override
- Widget build(BuildContext context) {
-   return Scaffold(
-     
-     body: Center(
-    child: _pages.elementAt(_selectedIndex), //New
-  ),
-     bottomNavigationBar: BottomNavigationBar(
-       backgroundColor: Color.fromRGBO(235 , 235, 235, 1),
-       showUnselectedLabels: false,
-       currentIndex: _selectedIndex, 
-  onTap: _onItemTapped,         
-       items: [
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _pages.elementAt(_selectedIndex), //New
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromRGBO(235, 235, 235, 1),
+        showUnselectedLabels: false,
+          selectedItemColor: Colors.white,
+
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
           BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/images/appoinment.png',
-                width: 32,
-                height: 32,
-              ),
-              activeIcon: Image.asset(
-                'assets/images/appoinment.png',
-                width: 32,
-                height: 32,
-                color: Color.fromRGBO(118, 123, 128, 1),
-              ),
-              label: 'Appoinments',
+            icon: Image.asset(
+              'assets/images/Document@3x.png',
+              width: 32,
+              height: 32,
             ),
-             
-             BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/images/appoinment.png',
-                width: 32,
-                height: 32,
-              ),
-              activeIcon: Image.asset(
-                'assets/images/appoinment.png',
-                width: 32,
-                height: 32,
-                color: Color.fromRGBO(118, 123, 128, 1),
-              ),
-              label: 'Orders',
-            ), BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/images/appoinment.png',
-                width: 32,
-                height: 32,
-              ),
-              activeIcon: Image.asset(
-                'assets/images/appoinment.png',
-                width: 32,
-                height: 32,
-                color: Color.fromRGBO(118, 123, 128, 1),
-              ),
-              label: 'Notifications',
+            activeIcon: Image.asset(
+              'assets/images/Calendar Selected@3x.png',
+              width: 32,
+              height: 32,
+              // color: Color.fromRGBO(118, 123, 128, 1),
             ),
-       ],
-     ),
-   );
- }
+            label: 'Appoinments',
+            
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/images/Buy@3x.png',
+              width: 32,
+              height: 32,
+            ),
+            activeIcon: Image.asset(
+              'assets/images/Buy Selected@3x.png',
+              width: 32,
+              height: 32,
+              // color: Color.fromRGBO(118, 123, 128, 1),
+            ),
+            label: 'Orders',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/images/Notification@3x.png',
+              width: 32,
+              height: 32,
+            ),
+            activeIcon: Image.asset(
+              'assets/images/Notification@3x.png',
+              width: 32,
+              height: 32,
+              color: Colors.white,
+            ),
+            label: 'Notifications',
+          ),
+        ],
+      ),
+    );
+  }
 }
