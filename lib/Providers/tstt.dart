85256a67ec3dd5381b9b304129b67e48d8f3f0b7
Mustafa Ahmed <mahmed@badee.com.sa>
2021-09-06 02:35:50 -0700

    'invoice_edit'

--- lib/Providers/tstt.dart
+++ lib/Providers/tstt.dart
@@ -71,7 +71,7 @@ class Invoice {
   double get _total =>
       products.map<double>((p) => p.total).reduce((a, b) => a + b);
 
-  double get _grandTotal => _total * (1 + tax);
+  double get _grandTotal => _total /** (1 + tax)*/;
 
  // String? _logo;
 
@@ -89,9 +89,9 @@ class Invoice {
       pw.MultiPage(
         pageTheme: _buildTheme(
           pageFormat,
-           Font.ttf(await rootBundle.load("assets/fonts/Poppins-Regular.ttf")),
-          Font.ttf(await rootBundle.load("assets/fonts/Poppins-Regular.ttf")),
-          Font.ttf(await rootBundle.load("assets/fonts/Poppins-Regular.ttf")),
+           Font.ttf(await rootBundle.load("assets/fonts/Cairo-Regular.ttf")),
+          Font.ttf(await rootBundle.load("assets/fonts/Cairo-Regular.ttf")),
+          Font.ttf(await rootBundle.load("assets/fonts/Cairo-Regular.ttf")),
         ),
         header: _buildHeader,
         footer: _buildFooter,
@@ -361,13 +361,13 @@ class Invoice {
                   ],
                 ),
                 pw.SizedBox(height: 5),
-                pw.Row(
+                /*pw.Row(
                   mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
                   children: [
                     pw.Text('Tax:'),
                     pw.Text('${(tax * 100).toStringAsFixed(1)}%'),
                   ],
-                ),
+                ),*/
                 pw.Divider(color: accentColor),
                 pw.DefaultTextStyle(
                   style: pw.TextStyle(
@@ -441,50 +441,54 @@ class Invoice {
       'Total'
     ];
 
-    return pw.Table.fromTextArray(
-      border: null,
-      cellAlignment: pw.Alignment.centerLeft,
-      headerDecoration: pw.BoxDecoration(
-        borderRadius: const pw.BorderRadius.all(pw.Radius.circular(2)),
-        color: baseColor,
-      ),
-      headerHeight: 25,
-      cellHeight: 40,
-      cellAlignments: {
-        0: pw.Alignment.centerLeft,
-        1: pw.Alignment.centerLeft,
-        2: pw.Alignment.centerRight,
-        3: pw.Alignment.center,
-        4: pw.Alignment.centerRight,
-      },
-      headerStyle: pw.TextStyle(
-        color: _baseTextColor,
-        fontSize: 10,
-        fontWeight: pw.FontWeight.bold,
-      ),
-      cellStyle: const pw.TextStyle(
-        color: _darkColor,
-        fontSize: 10,
-      ),
-      rowDecoration: pw.BoxDecoration(
-        border: pw.Border(
-          bottom: pw.BorderSide(
-            color: accentColor,
-            width: .5,
+    return pw.Directionality(
+      textDirection: pw.TextDirection.rtl,
+      child: pw.Table.fromTextArray(
+
+        border: null,
+        cellAlignment: pw.Alignment.centerLeft,
+        headerDecoration: pw.BoxDecoration(
+          borderRadius: const pw.BorderRadius.all(pw.Radius.circular(2)),
+          color: baseColor,
+        ),
+        headerHeight: 25,
+        cellHeight: 40,
+        cellAlignments: {
+          0: pw.Alignment.centerLeft,
+          1: pw.Alignment.centerLeft,
+          2: pw.Alignment.centerRight,
+          3: pw.Alignment.center,
+          4: pw.Alignment.centerRight,
+        },
+        headerStyle: pw.TextStyle(
+          color: _baseTextColor,
+          fontSize: 10,
+          fontWeight: pw.FontWeight.bold,
+        ),
+        cellStyle: const pw.TextStyle(
+          color: _darkColor,
+          fontSize: 10,
+        ),
+        rowDecoration: pw.BoxDecoration(
+          border: pw.Border(
+            bottom: pw.BorderSide(
+              color: accentColor,
+              width: .5,
+            ),
           ),
         ),
-      ),
-      headers: List<String>.generate(
-        tableHeaders.length,
-            (col) => tableHeaders[col],
-      ),
-      data: List<List<String>>.generate(
-        products.length,
-            (row) => List<String>.generate(
+        headers: List<String>.generate(
           tableHeaders.length,
-              (col) => products[row].getIndex(col),
+              (col) => tableHeaders[col],
         ),
-      ),
+        data: List<List<String>>.generate(
+          products.length,
+              (row) => List<String>.generate(
+            tableHeaders.length,
+                (col) => products[row].getIndex(col),
+          ),
+        ),
+      )
     );
   }
 }
